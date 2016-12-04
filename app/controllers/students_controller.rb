2544997d5b8b71fa1231d5a212287fb1f23c69f1
Rhys <rbrooker105@gmail.com>
2016-12-04 20:07:56 +1100

    add export method to student model and controller respond to csv format

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,11 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @students.as_csv }
+    end
   end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Student < ApplicationRecord
 
   belongs_to :school
@@ -6,5 +8,21 @@ class Student < ApplicationRecord
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+
+  def self.all_with_school_details
+    Student.select("students.*, schools.name as school_name, schools.address as school_address").joins(:school)
+  end
+
+  def self.as_csv
+    CSV.generate do |csv|
+      columns = %w(id first_name last_name DOB school_name school_address)
+      csv << columns.map(&:humanize)
+      all_with_school_details.each do |student|
+        csv << student.attributes.values_at(*columns)
+      end
+    end
+  end
+
 
 end
